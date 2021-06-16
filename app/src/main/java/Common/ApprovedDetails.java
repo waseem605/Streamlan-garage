96d25fd1026b19a002f61f8d9312a33de39449fd
Huzaifa Sammar <hm101326@gmail.com>
2021-06-17 00:28:46 +0500

    kafiActivity

---
+++ app/src/main/java/Common/ApprovedDetails.java
@@ -0,0 +1,24 @@
+package Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.streamlangarage.R;
+
+public class ApprovedDetails extends AppCompatActivity {
+   Button btn;
+   TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approved_details);
+        btn=(Button)findViewById(R.id.btnvehicleservice);
+        tv=(TextView)findViewById(R.id.diagnose);
+        tv.setText("Approved");
+        tv.setTextColor(getResources().getColor(R.color.green));
+        btn.setText("Message");
+    }
+}
