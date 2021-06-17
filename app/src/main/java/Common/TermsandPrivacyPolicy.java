f83ef08be0110ccfd101f433a51c55fa1e76690a
Huzaifa Sammar <hm101326@gmail.com>
2021-06-17 16:28:39 +0500

    kafiActivity

---
+++ app/src/main/java/Common/TermsandPrivacyPolicy.java
@@ -0,0 +1,16 @@
+package Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.streamlangarage.R;
+
+public class TermsandPrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termsand_privacy_policy);
+    }
+}
