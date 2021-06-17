83663eb496699a38e2ba1ce26e9d955982cbc8f8
Huzaifa Sammar <hm101326@gmail.com>
2021-06-17 13:45:41 +0500

    kafiActivity

--- app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/Approved.java
+++ app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/Approved.java
@@ -7,11 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.streamlangarage.R;
 
 public class Approved extends Fragment {
-
+    TextView mservice,mdiagnose;
+    Button mbtn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,13 @@ public class Approved extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_approved, container, false);
+        View v= inflater.inflate(R.layout.fragment_open_request, container, false);
+        mservice=(TextView)v.findViewById(R.id.service);
+        mservice.setVisibility(View.INVISIBLE);
+        mdiagnose=(TextView)v.findViewById(R.id.diagnose);
+        mdiagnose.setVisibility(View.INVISIBLE);
+        mbtn=(Button)v.findViewById(R.id.btnopenrequest);
+        mbtn.setText("View Details");
+        return v;
     }
 }
--- app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/OpenRequest.java
+++ app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/OpenRequest.java
@@ -7,13 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.streamlangarage.R;
 
 
 public class OpenRequest extends Fragment {
-
-
+    TextView mservice,mdiagnose;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +24,11 @@ public class OpenRequest extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_open_request, container, false);
+        View v= inflater.inflate(R.layout.fragment_open_request, container, false);
+        mservice=(TextView)v.findViewById(R.id.service);
+        mservice.setVisibility(View.INVISIBLE);
+        mdiagnose=(TextView)v.findViewById(R.id.diagnose);
+        mdiagnose.setVisibility(View.INVISIBLE);
+        return v;
     }
 }
