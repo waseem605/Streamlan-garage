c0b4d0afc7ce1d65396f22c19a48d88f371953ee
Huzaifa Sammar <hm101326@gmail.com>
2021-06-19 01:04:59 +0500

    kafiActivity

---
+++ app/src/main/java/Common/Chat_Details.java
@@ -0,0 +1,16 @@
+package Common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.streamlangarage.R;
+
+public class Chat_Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+    }
+}
