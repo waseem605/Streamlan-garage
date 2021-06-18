7dc85d3c5997ba832e2e06cb1168594b4d5b616e
Huzaifa Sammar <hm101326@gmail.com>
2021-06-18 21:01:39 +0500

    kafiActivity

--- app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/InProgress.java
+++ app/src/main/java/com/example/streamlangarage/Common/Fragments/Navigations/InProgress.java
@@ -8,11 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.streamlangarage.R;
 
 public class InProgress extends Fragment {
    LinearLayout btnlinearLayout;
+   RelativeLayout scheduleRL;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -25,6 +27,8 @@ public class InProgress extends Fragment {
         View v= inflater.inflate(R.layout.fragment_in_progress, container, false);
         btnlinearLayout=(LinearLayout)v.findViewById(R.id.llbtnsampleopenrequest);
         btnlinearLayout.setVisibility(View.INVISIBLE);
+        scheduleRL=(RelativeLayout)v.findViewById(R.id.Rlsamplecarschedule);
+        scheduleRL.setBackgroundDrawable(getActivity().getDrawable(R.drawable.white_background_b_round));
         return v;
     }
 }
